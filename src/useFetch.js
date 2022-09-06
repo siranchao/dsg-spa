1cc347a1036ace616d4934d14baae356f22b5091
Siranchao <scao34@myseneca.ca>
2022-09-06 16:24:42 -0400

    add fetch hook

---
+++ src/useFetch.js
@@ -0,0 +1,38 @@
+//Custom Hook
+
+import { useState, useEffect } from 'react'
+
+//use fake json REST api
+//when App is rendering, useEffect hook to fetch data
+// url: 'https://jsonplaceholder.typicode.com/users'
+
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPending, setIspPending] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok)
+                        throw Error(`could not fetch data from resource`)
+                    return res.json()
+                })
+                .then(data => {
+                    setData(data)
+                    setIspPending(false)
+                    setError(null)
+                })
+                .catch(err => {
+                    setIspPending(false)
+                    setError(err.message)
+                })
+        }, 5000)
+    }, [url])
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
